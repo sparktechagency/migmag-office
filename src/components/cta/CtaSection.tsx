365b80378262bc63104bf57ac0fa0213bd296e16
ishanrana363 <ishanrana363@gmail.com>
2025-12-02 10:31:43 +0600

    footer logo change

--- src/components/cta/CtaSection.tsx
+++ src/components/cta/CtaSection.tsx
@@ -6,20 +6,26 @@ const CtaSection = () => {
         <div>
             <section className="lg:mb-16 mb-10">
                 <div
-                    className="  bg-no-repeat bg-cover bg-center py-6 lg:pt-11 lg:pb-16 mt-16 relative overflow-hidden"
+                    className="bg-no-repeat bg-cover bg-center py-6 lg:pt-11 lg:pb-16 mt-16 relative overflow-hidden"
+                    style={{
+                        backgroundImage: "url('/images/cta/cta-image.png')"
+                    }}
                 >
-                    {/* <div className="absolute inset-0 bg-gradient-to-b from-black to-black/30 z-0"></div> */}
+                    {/* Optional Overlay */}
+                    {/* <div className="absolute inset-0 bg-black/40 z-0"></div> */}
 
                     <div className="relative lg:mt-20 mt-10 z-10 text-center">
-                        <h1 className="text-  font-bold lg:text-3xl text-lg">
+                        <h1 className="font-bold lg:text-3xl text-lg text-black">
                             Become a TuneM Artist.
                         </h1>
+
                         <p className="text-black leading-6 lg:text-xl font-thin max-w-[482px] mx-auto mt-3 lg:mt-9">
                             We work with talented singers and songwriters ready to take their voice further.
                             Expand your audience, get discovered, and join our global music network.
                         </p>
+
                         <Link href="/tune-m-artist">
-                            <button className="cursor-pointer text-black  text-sm px-4 py-2 border border-black rounded-2xl mx-auto mt-4 lg:mt-12">
+                            <button className="cursor-pointer text-black text-sm px-4 py-2 border border-black rounded-2xl mx-auto mt-4 lg:mt-12">
                                 GET STARTED
                             </button>
                         </Link>
